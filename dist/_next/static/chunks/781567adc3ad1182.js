1af04d0a66a744da6afcef2441842030522d1939
tchesa <cesarolimpio@gmail.com>
2025-10-03 19:36:00 -0300

    use vite

--- dist/_next/static/chunks/781567adc3ad1182.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/8597b0a5ae4e829b.js",
-  "static/chunks/turbopack-4c30fecb7fa81b79.js"
-])
--- dist/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"xVr6kmoh1+MpZGrySv1I/GhNd2nsASH2rWP0lP7FAHo=\"\n}"
